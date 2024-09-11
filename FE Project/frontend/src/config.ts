46b86aaf4155231a30c106ee4866d2b38210828b
ania475 <92125814+ania475@users.noreply.github.com>
2024-09-11 14:13:21 +0100

    'Deleted old FE project folder as I renamed it'

--- FE Project/frontend/src/config.ts
+++
@@ -1,6 +0,0 @@
-// config.ts
-const config = {
-    apiUrl: process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/get-user/get-user/',
-  };
-  
-  export default config;
--- FE Project/frontend/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.jpg';
-declare module '*.png';
-declare module '*.mp4' {
-    const src: string;
-    export default src;
-  }
